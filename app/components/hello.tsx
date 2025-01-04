05fb12848c7ad16ce5f6064b1584731e7e1756fb
Balaji Venkatraman <rslbalaji@gmail.com>
2025-01-04 22:37:08 +0530

    understand server and client side components

---
+++ app/components/hello.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React, {JSX} from "react";
+
+const Hello: React.FC = (): JSX.Element => {
+    console.log("client component");
+    return <div>Client Side component</div>;
+};
+
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+import Hello from "@/app/components/hello";
+
 const Home: React.FC = () => {
+    console.log("Home page");
     return (
-        <h1 className="text-3xl">Welcome to Next.JS</h1>
+        <>
+            <h1 className="text-3xl">Welcome to Next.JS</h1>
+            <Hello/>
+        </>
     );
 }
 
